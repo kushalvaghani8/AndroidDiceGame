9027f72da7688df92579a77cef58c3624a92be31
kushal <kushal_vaghani@yahoo.com>
2022-04-14 15:31:58 -0400

    added few conditions and finally ready to submit

--- app/src/main/java/com/example/androiddicegame/MainActivity.java
+++ app/src/main/java/com/example/androiddicegame/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.androiddicegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.autofill.AutofillValue;
@@ -12,8 +15,13 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import org.w3c.dom.Text;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -24,12 +32,15 @@ public class MainActivity extends AppCompatActivity {
     EditText addDiceText;
     TextView historyLabel, resultLabel;
     Spinner dice_spinner;
-    Integer dice[] = new Integer[]{ 2, 4, 6, 8, 10, 12, 20};
+
+
+    Integer dice[] = new Integer[]{ 2, 4, 6, 8, 10, 12, 20}; //adding the default dice values
+
     String result[] = {};
 
     ArrayList diceList = new ArrayList(Arrays.asList(dice));
 
-    ArrayList resultList = new ArrayList(Arrays.asList(result));
+    ArrayList resultList = new ArrayList(Arrays.asList(result)); //result string for saving it to history
 
 
     @Override
@@ -54,23 +65,25 @@ public class MainActivity extends AppCompatActivity {
         addDiceBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 if (addDiceText.getText().toString().equals("")) {
                     Toast.makeText(getApplicationContext(),"Oops forgot to enter a number, try again",Toast.LENGTH_SHORT).show();
+                } else if (addDiceText.getText().toString().equals("0") || addDiceText.getText().toString().equals("00") || addDiceText.getText().toString().equals("000")|| addDiceText.getText().toString().equals("0000")|| addDiceText.getText().toString().equals("00000") || addDiceText.getText().toString().equals("000000")) {
+                    Toast.makeText(getApplicationContext(),"😱 Really!!!, try again",Toast.LENGTH_SHORT).show();
+                    addDiceText.setText("");
                 } else {
                     int diceText = Integer.parseInt(String.valueOf(addDiceText.getText()));
-
-                    if (!diceList.contains(diceText)) {
-                        diceList.add(diceText);
-                        adapter.notifyDataSetChanged();
-                        dice_spinner.setAdapter(adapter);
-                        int spinnerPosition = adapter.getPosition(diceText);
-                        dice_spinner.setSelection(spinnerPosition);
+                    if (!diceList.contains(diceText)){ //checking for 0 values & adding the number only if its not in the list
+                        diceList.add(diceText); //adding the items to list
+                        adapter.notifyDataSetChanged(); //letting the spinner adapter know about data change to let them add new item
+                        dice_spinner.setAdapter(adapter); //setting spinner adapter
+                        int spinnerPosition = adapter.getPosition(diceText);  //getting the spinner position/number
+                        dice_spinner.setSelection(spinnerPosition); //setting the spinner position to newly added number
                     }
-                    addDiceText.setText("");
+                    addDiceText.setText(""); //clearing the text field
                 }
            }
         });
-
 
 
                 /*----------------------------------------------
@@ -81,30 +94,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Die d = new Die();
-               int noOfSides = (int) dice_spinner.getSelectedItem();
-                d.SetNoOfSides(noOfSides);
+                Die d = new Die(); //constructor for die class
+               int noOfSides = (int) dice_spinner.getSelectedItem(); // getting selected value from spinner
+                d.SetNoOfSides(noOfSides); //passing the number of sides to die class
                d.roll();
 
-                 Integer currentSide = d.GetCurrentSide();
-                 if (noOfSides == currentSide) {
-                     resultLabel.setTextSize(18);
-                     resultLabel.setText("Congratulations!!! You've got highest number " + currentSide.toString());
-                     resultList.add(currentSide.toString());
-                     historyLabel.setText("History: " + resultList);
-                 }
+                 Integer currentSide = d.GetCurrentSide(); //getting current side
+                resultLabel.setTextSize(18);//
+                if (noOfSides == currentSide) { //checking for conditions and if the number is the highest showing a message
+                    resultLabel.setText("Congratulations!!! You've got highest number " + currentSide.toString());
+                }
                  else {
-                     resultLabel.setTextSize(18);
-                     resultLabel.setText(currentSide.toString());
-                     resultList.add(currentSide.toString());
-                     historyLabel.setText("History: " + resultList);
-                 }
+                    resultLabel.setText(currentSide.toString()); //no message if its not the highest number
+                }
+                resultList.add(currentSide.toString());
+                historyLabel.setText("History: " + resultList); //adding all results to list and displaying it to history label
             }
         });
 
          /*----------------------------------------------
                             Rolling the dice twice
-                 -----------------------------------------------*/
+          -----------------------------------------------*/
         spin2btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -117,8 +127,8 @@ public class MainActivity extends AppCompatActivity {
                 Integer currentSide1 = d.GetCurrentSide();
                 Integer currentSide2 = d.GetNewCurrentSide();
 
-                if (noOfSides == currentSide1 && noOfSides == currentSide2) {
-                    resultLabel.setTextSize(14);
+                if (noOfSides == currentSide1 && noOfSides == currentSide2) { //checking if both dice is same and of highest number
+                    resultLabel.setTextSize(14); //setting the font to smaller size and displaying the message
                     resultLabel.setText("Congratulations!!! You've got highest number for both " + currentSide1.toString() + ", " + currentSide2.toString());
                 }
                 else {
@@ -132,5 +142,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
     }
+
+
 }
